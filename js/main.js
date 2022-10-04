c7b5ec7e934df44fa2106198650b25e556c9d360
Irina <irenquick@gmail.com>
2022-10-04 17:15:20 +0300

    исправляет условия в функции

--- js/main.js
+++ js/main.js
@@ -2,21 +2,11 @@
 //1. Проверить наличине аргумента text, проверить typeof аргумента, что это строка
 //2. Проверить наличие второго аргумента length, что это число, проверить, что это не отрицательно число
 
-function maxTextLength (inputtext, length) {
-  if (typeof inputtext === 'string' && typeof length === 'number') {
-    console.log (length);
+function check (inputtext, length) {
+  if (typeof inputtext === 'string' && typeof length === 'number' && inputtext.length <= length) {
+    return true;
   } else {
     return false;
-  };
-
-  if (inputtext.length < 20) {
-  return false;
-  } else if (inputtext.length >= 140) {
-    return false;
-    } else {
-      return true;
-    }
+  }
 }
-  
-  
-  maxTextLength();
+check();
